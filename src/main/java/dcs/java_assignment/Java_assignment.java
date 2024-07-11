50758b7ac680610543936fb12315a4dce90e07a3
TzPing <zceping@gmail.com>
2024-07-11 19:43:29 +0800

    print a sentences

---
+++ src/main/java/dcs/java_assignment/Java_assignment.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package dcs.java_assignment;
+
+/**
+ *
+ * @author TzPing
+ */
+public class Java_assignment {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
